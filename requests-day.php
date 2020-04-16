a7bdc02a0ed920d141a6a4bb1ef27eb0c70b6c82
spamty <spamty@uberspace>
2020-04-16 21:35:12 +0200

    final

--- requests-day.php
+++ requests-day.php
@@ -3,18 +3,69 @@
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Requests per Day</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-          <button type="button" class="btn btn-sm btn-outline-secondary">
-            <span data-feather="calendar"></span>
-            From 20200415 to 20200415
-          </button>
+          <a href="requests-day.php?range=7" role="button" class="btn btn-sm btn-outline-secondary">
+            <span data-feather="calendar"></span> 7 days
+          </a>
+          <a href="requests-day.php?range=14" role="button" class="btn btn-sm btn-outline-secondary">
+            <span data-feather="calendar"></span> 14 days
+          </a>
         </div>
       </div>
 
 
-<pre><code>SELECT COUNT(*) FROM `tracking` WHERE `date` = '20200415'  AND (`statusReturned` != '1' AND `statusReturned` != '200')</code></pre>
-<pre><code>SELECT COUNT(*) FROM `tracking` WHERE `date` = '20200415'  AND (`statusReturned` =  '1' OR  `statusReturned` =  '200')</code></pre>
+
+<?php 
+// get connection details
+require_once dirname(__FILE__)."/../dbconnect/readonly.php";
+// connect to mysql db
+try{ $db = new PDO('mysql:host='.MYSQLHOST.';dbname=spamty_api', MYSQLUSER, MYSQLPASS); }
+catch(PDOException $e){ exit("Error while connecting to database."); }
 
 
+
+
+// default 7 days:
+$displayDates = array();
+$displayDates[]= date("Ymd", time());
+$displayDates[]= date("Ymd", time() - 60*60*24);
+$displayDates[]= date("Ymd", time() - 60*60*24*2);
+$displayDates[]= date("Ymd", time() - 60*60*24*3);
+$displayDates[]= date("Ymd", time() - 60*60*24*4);
+$displayDates[]= date("Ymd", time() - 60*60*24*5);
+$displayDates[]= date("Ymd", time() - 60*60*24*6);
+// if 14 days:
+if( $_GET['range'] == 14 ){
+ $displayDates[]= date("Ymd", time() - 60*60*24*7);
+ $displayDates[]= date("Ymd", time() - 60*60*24*8);
+ $displayDates[]= date("Ymd", time() - 60*60*24*9);
+ $displayDates[]= date("Ymd", time() - 60*60*24*10);
+ $displayDates[]= date("Ymd", time() - 60*60*24*11);
+ $displayDates[]= date("Ymd", time() - 60*60*24*12);
+ $displayDates[]= date("Ymd", time() - 60*60*24*13);
+}
+// The data we have:  $displayDates = 
+//  Array ( [0] => 20200416 [1] => 20200415 [2] => 20200414 [3] => 20200413 [4] => 20200412 [5] => 20200411 [6] => 20200410 ) 
+
+
+
+// graph data
+$graphData = array();
+
+foreach( $displayDates as $displayCur ){
+	$dbQuery = $db->prepare("SELECT COUNT(*) FROM `tracking` WHERE `date` = '".$displayCur."' AND (`statusReturned`='1' OR `statusReturned`='200')");
+	$dbQuery->execute($dbExecData);
+	$dbD = $dbQuery->fetch(PDO::FETCH_BOTH);
+	$graphData[$displayCur] = $dbD[0];
+
+}
+// The db data we get:  $graphData = 
+//   Array ( [20200416] => 14 [20200415] => 22 [20200414] => 9 [20200413] => 24 [20200412] => 7 [20200411] => 3 [20200410] => 14 )
+
+?>
+
+
+
+<!-- Chart -->
 <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
@@ -36,40 +87,22 @@
     type: 'line',
     data: {
       labels: [
-        '20190418',
-        '20190419',
-        '20190420',
-        '20190421',
-        '20190422'
+	<?php foreach( $displayDates as $displayCur ){
+		echo "'".$displayCur."',";
+	} ?>
       ],
       datasets: [{
         data: [
-          24,
-          27,
-          9,
-          14,
-          15
+	<?php foreach( $graphData as $graphCur ){
+		echo $graphCur.",";
+	} ?>
         ],
-	label: 'Successful requests',
+	label: 'Requests',
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
         borderWidth: 4,
         pointBackgroundColor: '#007bff'
-      },{
-        data: [
-          3,
-          2,
-          1,
-          4,
-          3
-        ],
-	label: 'Errors',
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#721c24',
-        borderWidth: 4,
-        pointBackgroundColor: '#721c24'
       }]
     },
     options: {
@@ -81,7 +114,7 @@
         }]
       },
       legend: {
-        display: true
+        display: false
       }
     }
   })
